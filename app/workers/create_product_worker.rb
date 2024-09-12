5ea711c9cf384d2920e57772d60fef736c884de8
Leo Bazan <leo.bazan.x@gmail.com>
2024-09-12 15:11:50 -0300

    fix: CreateProductWorker performance
    Using end-to-end loading tests, a performance issue was detected.
    Test: 5000 concurrent request creating asyinc jobs - 100% error in jobs
    
    The issue was origin by STDOUT called by :puts calls inside the worker.
    
    Errno::EIO: Input/output error @ io_writev - <STDOUT>

--- app/workers/create_product_worker.rb
+++ app/workers/create_product_worker.rb
@@ -4,8 +4,8 @@ class CreateProductWorker
   include Sidekiq::Worker
 
   def perform(product_name)
-    puts "Enqueued job to create product: #{product_name}"
+    logger.info "Enqueued job to create product: #{product_name}"
     ProductService.create_product(product_name)
-    puts "Created product: #{product_name}"
+    logger.info "Created product: #{product_name}"
   end
 end
